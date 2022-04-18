a622967a5b5856965a73f128a8a090c44880e83f
Ivan Plotnikov <plotnikov@protonmail.com>
2022-04-19 00:04:05 +0300

    Initial project version

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "lib.h"
+
+#include <iostream>
+
+int main(int, char **) {
+    std::cout << "Homework 02" << std::endl;
+	std::cout << "Version: " << version() << std::endl;
+
+
+	return 0;
+}
